5df98600150fd8f32bbf4759b4b07aecb867cae2
深度学习 <33132228+Kucashu@users.noreply.github.com>
2019-09-22 17:48:39 +0800

    添加项目文件。

---
+++ TrojanClientSlim/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace TrojanClientSlim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Exit = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.isSock5 = new System.Windows.Forms.CheckBox();
+            this.RemoteAddressBox = new System.Windows.Forms.TextBox();
+            this.RemotePortBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PasswordBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.isHttp = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.isVerifyCert = new System.Windows.Forms.CheckBox();
+            this.isVerifyHostname = new System.Windows.Forms.CheckBox();
+            this.Run = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(214, 158);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 1;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Cancle_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(110, 158);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 2;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Remote Address:";
+            // 
+            // isSock5
+            // 
+            this.isSock5.AutoSize = true;
+            this.isSock5.Checked = true;
+            this.isSock5.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.isSock5.Enabled = false;
+            this.isSock5.Location = new System.Drawing.Point(113, 102);
+            this.isSock5.Name = "isSock5";
+            this.isSock5.Size = new System.Drawing.Size(54, 16);
+            this.isSock5.TabIndex = 4;
+            this.isSock5.Text = "Sock5";
+            this.isSock5.UseVisualStyleBackColor = true;
+            // 
+            // RemoteAddressBox
+            // 
+            this.RemoteAddressBox.Location = new System.Drawing.Point(113, 15);
+            this.RemoteAddressBox.Name = "RemoteAddressBox";
+            this.RemoteAddressBox.Size = new System.Drawing.Size(175, 21);
+            this.RemoteAddressBox.TabIndex = 5;
+            // 
+            // RemotePortBox
+            // 
+            this.RemotePortBox.Location = new System.Drawing.Point(113, 42);
+            this.RemotePortBox.Name = "RemotePortBox";
+            this.RemotePortBox.Size = new System.Drawing.Size(175, 21);
+            this.RemotePortBox.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Remote Port:";
+            // 
+            // PasswordBox
+            // 
+            this.PasswordBox.Location = new System.Drawing.Point(113, 69);
+            this.PasswordBox.Name = "PasswordBox";
+            this.PasswordBox.PasswordChar = '*';
+            this.PasswordBox.Size = new System.Drawing.Size(175, 21);
+            this.PasswordBox.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Password:";
+            // 
+            // isHttp
+            // 
+            this.isHttp.AutoSize = true;
+            this.isHttp.Checked = true;
+            this.isHttp.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.isHttp.Location = new System.Drawing.Point(216, 103);
+            this.isHttp.Name = "isHttp";
+            this.isHttp.Size = new System.Drawing.Size(48, 16);
+            this.isHttp.TabIndex = 10;
+            this.isHttp.Text = "HTTP";
+            this.isHttp.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Proxy:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Verify";
+            // 
+            // isVerifyCert
+            // 
+            this.isVerifyCert.AutoSize = true;
+            this.isVerifyCert.Checked = true;
+            this.isVerifyCert.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.isVerifyCert.Location = new System.Drawing.Point(113, 126);
+            this.isVerifyCert.Name = "isVerifyCert";
+            this.isVerifyCert.Size = new System.Drawing.Size(48, 16);
+            this.isVerifyCert.TabIndex = 12;
+            this.isVerifyCert.Text = "Cert";
+            this.isVerifyCert.UseVisualStyleBackColor = true;
+            // 
+            // isVerifyHostname
+            // 
+            this.isVerifyHostname.AutoSize = true;
+            this.isVerifyHostname.Checked = true;
+            this.isVerifyHostname.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.isVerifyHostname.Location = new System.Drawing.Point(216, 126);
+            this.isVerifyHostname.Name = "isVerifyHostname";
+            this.isVerifyHostname.Size = new System.Drawing.Size(72, 16);
+            this.isVerifyHostname.TabIndex = 14;
+            this.isVerifyHostname.Text = "Hostname";
+            this.isVerifyHostname.UseVisualStyleBackColor = true;
+            // 
+            // Run
+            // 
+            this.Run.Location = new System.Drawing.Point(12, 158);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(75, 23);
+            this.Run.TabIndex = 15;
+            this.Run.Text = "Run";
+            this.Run.UseVisualStyleBackColor = true;
+            this.Run.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 201);
+            this.Controls.Add(this.Run);
+            this.Controls.Add(this.isVerifyHostname);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.isVerifyCert);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.isHttp);
+            this.Controls.Add(this.PasswordBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.RemotePortBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.RemoteAddressBox);
+            this.Controls.Add(this.isSock5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Exit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "TCS v1.0 by Kevin";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox isSock5;
+        private System.Windows.Forms.TextBox RemoteAddressBox;
+        private System.Windows.Forms.TextBox RemotePortBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PasswordBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox isHttp;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox isVerifyCert;
+        private System.Windows.Forms.CheckBox isVerifyHostname;
+        private System.Windows.Forms.Button Run;
+    }
+}
+
---
+++ TrojanClientSlim/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace TrojanClientSlim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (isPortUsed(1080))
+            {
+                MessageBox.Show("Port 1080 is in use!\r\nTrojan may failure to work.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            if (isPortUsed(54392))
+            {
+                MessageBox.Show("Port 54392 is in use!\r\nPrivoxy may failure to work.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+            if (File.Exists("conf"))
+            {
+                string[] conf = Encrypt.DeBase64(File.ReadAllText("conf")).Split(':');
+                if (conf.Length == 5)
+                {
+                    this.RemoteAddressBox.Text = conf[0];
+                    this.RemotePortBox.Text = conf[1];
+                    this.PasswordBox.Text = conf[2];
+                    if (conf[3].ToLower() == "true")
+                    {
+                        isHttp.Checked = true;
+                    }
+                    if (conf[4].ToLower().Contains("c"))
+                    {
+                        isVerifyCert.Checked = true;
+                    }
+                    if (conf[4].ToLower().Contains("h"))
+                    {
+                        isVerifyHostname.Checked = true;
+                    }
+                }
+            }
+            else
+            {
+                File.Create("conf");
+            }
+
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            Proxy.UnsetProxy();
+            try
+            {
+                KillProcess();
+                MessageBox.Show("Kill trojan process successfully!", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                MessageBox.Show("Kill trojan process failed!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public class Encrypt
+        {
+            public static string Base64(string str)
+            {
+                try
+                {
+                    return Convert.ToBase64String((byte[])Encoding.Default.GetBytes(str));
+                }
+                catch
+                {
+                    throw new InvalidCastException();
+                }
+            }
+
+            public static string DeBase64(string str)
+            {
+                try
+                {
+                    return Encoding.Default.GetString((byte[])Convert.FromBase64String(str));
+                }
+                catch
+                {
+                    throw new InvalidCastException();
+                }
+            }
+        }
+
+        private void Cancle_Click(object sender, EventArgs e)
+        {
+            Proxy.UnsetProxy();
+            System.Environment.Exit(0);
+        }
+
+        private void Run_Click(object sender, EventArgs e)
+        {
+            string ch = string.Empty;
+            if (isVerifyCert.Checked == true)
+            {
+                ch += "c";
+            }
+            if (isVerifyHostname.Checked == true)
+            {
+                ch += "h";
+            }
+            File.WriteAllText("conf", Encrypt.Base64($"{RemoteAddressBox.Text}:{RemotePortBox.Text}:{PasswordBox.Text}:{isHttp.Checked}:{ch}"));
+            try
+            {
+                KillProcess();
+            }
+            catch
+            {
+                MessageBox.Show("Kill trojan process failed!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            GeneralTrojanConf();
+            RunTrojan();
+            if (isHttp.Checked == true)
+            {
+                
+                RunPivoxy();
+                Proxy.SetProxy("127.0.0.1:54392");
+
+            }
+            MessageBox.Show("Trojan run successfully!", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        private void KillProcess()
+        {
+
+            Process[] myproc = Process.GetProcesses();
+            foreach (Process item in myproc)
+            {
+                if (item.ProcessName == "trojan" || item.ProcessName == "privoxy")
+                {
+                    item.Kill();
+                }
+            }
+        }
+
+        private void RunTrojan()
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = @"trojan\trojan.exe";
+            p.StartInfo.Arguments = @"-c trojan.conf";
+            p.StartInfo.UseShellExecute = false;
+            //p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            p.Dispose();
+
+        }
+
+        private void RunPivoxy()
+        {
+            
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            //pc.StartInfo.Arguments = $"start {path}\\privoxy\\privoxy.exe {path}\\privoxy\\config.txt";
+            p.StartInfo.Arguments = "/c START /MIN privoxy\\privoxy.exe privoxy\\config.txt";
+            p.StartInfo.UseShellExecute = false;
+            //p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            p.Dispose();
+        }
+
+        private void GeneralTrojanConf()
+        {
+            //if (File.Exists("trojan.conf"))
+            //{
+                File.WriteAllText("trojan.conf", "{\"run_type\": \"client\", \"local_addr\": \"127.0.0.1\", \"local_port\": 1080, \"remote_addr\":\"" +
+                    RemoteAddressBox.Text + "\", \"remote_port\": " + RemotePortBox.Text + ", \"password\": [\"" + PasswordBox.Text + "\"], \"log_level\": 1, \"ssl\": { \"verify\": " +
+                    isVerifyCert.Checked.ToString().ToLower() + ",\"verify_hostname\": " + isVerifyHostname.Checked.ToString().ToLower() + ", \"cert\": \"\", \"cipher\": \"ECDHE - ECDSA - AES128 - GCM - SHA256:" +
+                    "ECDHE - RSA - AES128 - GCM - SHA256:ECDHE - ECDSA - AES256 - GCM - SHA384:ECDHE - RSA - AES256 - GCM - SHA384:ECDHE - ECDSA - CHACHA20 - POLY1305:" +
+                    "ECDHE - RSA - CHACHA20 - POLY1305:ECDHE - RSA - AES128 - SHA:ECDHE - RSA - AES256 - SHA:RSA - AES128 - GCM - SHA256:RSA - AES256 - GCM - SHA384:" +
+                    "RSA - AES128 - SHA:RSA - AES256 - SHA:RSA - 3DES - EDE - SHA\", \"sni\": \"\", \"alpn\": [ \"http / 1.1\" ], \"reuse_session\": true, \"session_ticket\": false," +
+                    " \"curves\": \"\" }, \"tcp\": { \"no_delay\": true, \"keep_alive\": true, \"fast_open\": false, \"fast_open_qlen\": 20 } }");
+            //}
+            //else
+            //{
+                //File.Create("")
+            //}
+        }
+
+
+
+        private static bool isPortUsed(int port)
+        {
+            bool isPortUsed = false;
+            IPGlobalProperties ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+            IPEndPoint[] ipEndPoints = ipProperties.GetActiveTcpListeners();
+            foreach (IPEndPoint endPoint in ipEndPoints)
+            {
+                if (endPoint.Port == port)
+                {
+                    isPortUsed = true;
+                    break;
+                }
+            }
+            return isPortUsed;
+        }
+
+    }
+}
