59fcbeb85eee6ae1f1729171e75962f6c4f29ae5
深度学习 <33132228+Kucashu@users.noreply.github.com>
2019-09-30 21:49:43 +0800

    增加图标

--- TrojanClientSlim/Program.cs
+++ TrojanClientSlim/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TrojanClientSlim
